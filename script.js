3d14ebcb1fc66f24d4ca11045c32199d0f6f1bda
DavidvK17 <david.sing7@gmail.com>
2022-01-27 18:34:20 +0100

    Implementating toCamelCase

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const testInput = document.getElementsByTagName('textarea')[0];
+const submitBtn = document.getElementById('submit');
+
+submitBtn.addEventListener('click', function(e) {
+  e.preventDefault();
+  toCamelCase();
+});
+
+function toCamelCase() {
+  const arr = testInput.value
+    .toLowerCase()
+    .split('\n')
+    .forEach(val => {
+      const [firstWord, secondWord] = val.trim().split('_');
+      console.log(
+        [firstWord, secondWord[0].toUpperCase(), secondWord.slice(1)].join('')
+      );
+    });
+}
